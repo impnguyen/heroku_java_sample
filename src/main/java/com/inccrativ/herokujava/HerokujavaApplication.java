be1e8c9873675dd3e4433f769f95d7eeb87e7672
Manh Phuoc Nguyen <imoney91@me.com>
2021-03-26 23:32:13 +0100

    init

---
+++ src/main/java/com/inccrativ/herokujava/HerokujavaApplication.java
@@ -0,0 +1,14 @@
+package com.inccrativ.herokujava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokujavaApplication {
+
+	public static void main(String[] args) {
+		System.out.println("started");
+		SpringApplication.run(HerokujavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inccrativ/herokujava/HerokujavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inccrativ.herokujava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokujavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
