5b5810991df6d929611d170463a65deefe75cd36
Manh Phuoc Nguyen <imoney91@me.com>
2021-03-27 00:05:55 +0100

    fixed mapping

--- src/main/java/com/inccrativ/herokujava/TestController.java
+++ src/main/java/com/inccrativ/herokujava/TestController.java
@@ -4,8 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping(path = "api/v1/student")
 public class TestController {
     
     @GetMapping("/hello")
