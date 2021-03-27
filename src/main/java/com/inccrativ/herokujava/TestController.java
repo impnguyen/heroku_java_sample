92e782588cc3e1ad56c8e437ba3e3a89fa4d348b
Manh Phuoc Nguyen <imoney91@me.com>
2021-03-27 10:45:56 +0100

    add translation feature

--- src/main/java/com/inccrativ/herokujava/TestController.java
+++ src/main/java/com/inccrativ/herokujava/TestController.java
@@ -2,6 +2,8 @@ package com.inccrativ.herokujava;
 
 
 import java.util.Map;
+import com.google.cloud.translate.*;
+
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpRequest;
@@ -10,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -32,6 +35,15 @@ public class TestController {
         // return new ResponseEntity<String>(
         //   String.format("Listed %d headers", headers.size()), HttpStatus.OK);
         return headers;
+    }
+
+    //how to call: 
+    @GetMapping("/translate")
+    public String translateForPro(@RequestParam String source) {
+        Translate translate = TranslateOptions.getDefaultInstance().getService();
+        Translation translation = translate.translate(source);
+
+        return "Translated to english:" + translation.getTranslatedText();
     }
 
 }
